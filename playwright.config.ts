bff282857ae97a7b35c0fa04dbe491174bb972d1
Ankit-QAble <148555575+Ankit-QAble@users.noreply.github.com>
2026-08-10 17:25:14 +0530

    Reduce timeout from 600000 to 300000 milliseconds

--- playwright.config.ts
+++ playwright.config.ts
@@ -5,7 +5,7 @@ import { GRID, getLambdaTestWsEndpoint } from './utils/lambdaGrid';
 const mobileDevices = require('./mobileDevices');
 
 // ─── SETTINGS ────────────────────────────────────────────────────────────────
-const TIMEOUT = 600000;
+const TIMEOUT = 300000;
 const RETRIES = 1;             // retries on failure
 const WORKERS = { ci: 2, local: 2 };
 const HEADED  = process.env.HEADED === 'true';
